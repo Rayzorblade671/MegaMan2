bf3d741f37a0975ffd974ed587e25e7711d53325
DESKTOP-KCCG16C\Katie <kesoter@student.fullsail.edu>
2018-01-14 16:55:48 -0500

    Re-organizing scripts and prefabs for easier access

---
+++ MegaMan2/Assets/Scripts/Flashy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("TileSelect", 0f, 0.14f);
+    }
+
+    void TileSelect()
+    {
+        if (this.GetComponent<MeshRenderer>().enabled == true)
+        {
+            this.GetComponent<MeshRenderer>().enabled = false;
+        }
+        else if (this.GetComponent<MeshRenderer>().enabled == false)
+        {
+            this.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+}
