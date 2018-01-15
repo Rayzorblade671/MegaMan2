48364f5b6fc1367abfdda437a9b5a90445ac8796
DESKTOP-E8EUTMQ\Nathan Geno <nathan.geno94@gmail.com>
2018-01-15 18:52:45 -0500

    Mostly finished character spawner

--- MegaMan2/Assets/Scripts/CharacterSpawner.cs
+++ MegaMan2/Assets/Scripts/CharacterSpawner.cs
@@ -1,16 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class CharacterSpawner : MonoBehaviour {
+public class CharacterSpawner : MonoBehaviour
+{
+    //Variables
+    [SerializeField] GameObject character;
+    [SerializeField] Camera mainCam;
+    [SerializeField] bool cameraCanSee = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float spawnDistance;
+    
+    //Accessors
+    public bool CameraCanSee
+    {
+        get {return cameraCanSee;}
+
+        set {cameraCanSee = value;}
+    }
+    
+    public GameObject Character
+    {
+        get { return character; }
+
+        set { character = value; }
+    }
+
+    public Camera MainCamera
+    {
+        get { return mainCam; }
+
+        set { mainCam = value; }
+    }
+
+    public float SpawnDistance
+    {
+        get { return spawnDistance; }
+
+        set { spawnDistance = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(spawnDistance);
+    }
+
 }
--- MegaMan2/Assets/Scripts/CharacterSpawnerStateMachine.cs
+++ MegaMan2/Assets/Scripts/CharacterSpawnerStateMachine.cs
@@ -1,16 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class CharacterSpawnerStateMachine : MonoBehaviour {
+public class CharacterSpawnerStateMachine : MonoBehaviour
+{
+    private GameObject spawnedCharacter;
+    private CharacterSpawner characterSpawner;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] bool isAlive = false;
+
+    Dictionary<CharacterSpawnerStates, Action> cssm = new Dictionary<CharacterSpawnerStates, Action>();
+
+    enum CharacterSpawnerStates
+    {
+        ON,
+        OFF
+    }
+
+    private CharacterSpawnerStates currentState = CharacterSpawnerStates.OFF;
+
+    void SetState(CharacterSpawnerStates newState)
+    {
+        currentState = newState;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        characterSpawner = GetComponent<CharacterSpawner>();
+
+        cssm.Add(CharacterSpawnerStates.ON, new Action(OnState));
+        cssm.Add(CharacterSpawnerStates.OFF, new Action(OffState));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(isAlive);
+        characterSpawner.SpawnDistance = characterSpawner.MainCamera.transform.position.x - gameObject.transform.position.x;
+
+        cssm[currentState].Invoke();
+    }
+
+    void OnState()
+    {   
+        Debug.Log("On");
+
+
+        if (characterSpawner.CameraCanSee == true && isAlive == false)
+        {
+            Instantiate(characterSpawner.Character, transform.position, transform.rotation);
+            spawnedCharacter = characterSpawner.Character;
+            //characterSpawner.Character.name = "Enemy " + spawnedCharacter + transform.position;
+            isAlive = true;
+            SetState(CharacterSpawnerStates.OFF);
+        }
+    }
+
+    void OffState()
+    {
+        Debug.Log("Off");
+
+        //GameObject newEnemy = GameObject.Find("Enemy " + transform.position + "(Clone)");
+        if (GameObject.Find(characterSpawner.Character.name + "(Clone)") == null)
+        {
+            isAlive = false;
+            Debug.Log(characterSpawner.Character.name);
+        }
+        else
+            isAlive = true;
+
+
+        if (characterSpawner.SpawnDistance >= 1.5 || characterSpawner.SpawnDistance <= -1.5)
+        {
+            characterSpawner.CameraCanSee = false;
+        }
+
+        if (characterSpawner.CameraCanSee == false && isAlive == false)
+        {
+            if (characterSpawner.SpawnDistance <= 1 && characterSpawner.SpawnDistance >= -1)
+            {
+                characterSpawner.CameraCanSee = true;
+                SetState(CharacterSpawnerStates.ON);
+            }
+        }
+    }
 }
