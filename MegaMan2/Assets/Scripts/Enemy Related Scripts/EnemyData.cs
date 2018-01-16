6b62fc389057077d0b81415e6eee72f864687bad
DESKTOP-E8EUTMQ\Nathan Geno <nathan.geno94@gmail.com>
2018-01-16 14:50:07 -0500

    Began working on Mole script; switched colliders on enemy objects to 2D; added new layer "Enemy"

---
+++ MegaMan2/Assets/Scripts/Enemy Related Scripts/EnemyData.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyData : MonoBehaviour
+{
+    //Variables
+    [SerializeField] int strength;
+    [SerializeField] int health;
+    [SerializeField] float moveSpeed;
+    [SerializeField] Transform target;
+
+    [SerializeField] Camera mainCam;
+    [SerializeField] bool cameraCanSee = false;
+
+    private Animator animator;
+    private float distanceToCam;
+
+    //Accessors
+    public int Strength
+    {
+        get { return strength; }
+
+        set{strength = value;}
+    }
+
+    public int Health
+    {
+        get { return health; }
+
+        set { health = value; }
+    }
+
+    public Transform Target
+    {
+        get { return target; }
+
+        set { target = value; }
+    }
+
+    public Camera MainCam
+    {
+        get { return mainCam; }
+
+        set { mainCam = value; }
+    }
+
+    public bool CameraCanSee
+    {
+        get { return cameraCanSee; }
+
+        set { cameraCanSee = value; }
+    }
+
+    public Animator Animator
+    {
+        get { return animator; }
+
+        set { animator = value; }
+    }
+
+    public float MoveSpeed
+    {
+        get { return moveSpeed; }
+
+        set { moveSpeed = value; }
+    }
+
+    public float DistanceToCam
+    {
+        get { return distanceToCam; }
+
+        set { distanceToCam = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToCam = mainCam.transform.position.x - gameObject.transform.position.x;
+    }
+}
---
+++ MegaMan2/Assets/Scripts/Enemy Related Scripts/MoleStateMachine.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoleStateMachine : MonoBehaviour
+{
+    //StateMachine Setup ---------------------------------------------------------------------
+    private EnemyData enemyData;
+
+    Dictionary<MoleStates, Action> msm = new Dictionary<MoleStates, Action>();
+
+    enum MoleStates
+    {
+        ALIVE,
+        DEAD
+    }
+
+    private MoleStates currentState = MoleStates.ALIVE;
+
+    void SetState(MoleStates newState)
+    {
+        currentState = newState;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        enemyData = GetComponent<EnemyData>();
+
+        enemyData.Health = 3;
+
+        msm.Add(MoleStates.ALIVE, new Action(AliveState));
+        msm.Add(MoleStates.DEAD, new Action(DeadState));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        msm[currentState].Invoke();
+    }
+
+    //States -----------------------------------------------------------------------------
+    void AliveState()
+    {
+        if (enemyData.DistanceToCam >= 16 || enemyData.DistanceToCam <= -16)
+        {
+            SetState(MoleStates.DEAD);
+        }
+    }
+
+    void DeadState()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
